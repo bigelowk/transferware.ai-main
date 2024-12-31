70717321ec0c22187f6c8945a5e1c898de3a96a9
bigelowk <112422927+bigelowk@users.noreply.github.com>
2024-12-30 21:28:04 -0500

    changes to settings

--- python_src/build/lib/transferwareai/config.py
+++ python_src/build/lib/transferwareai/config.py
@@ -3,5 +3,5 @@ from dynaconf import Dynaconf
 # Attempt to load a settings file *relative to the entrypoint python module*
 settings = Dynaconf(
     envvar_prefix="TRANSFERWARE",
-    settings_files=['settings.toml'],
+    settings_files=['settings.local.toml'],
 )
