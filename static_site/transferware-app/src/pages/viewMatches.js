42b220ed56e213ed42a3143907ed9928eebefee7
aleut07 <aleut@umich.edu>
2025-03-17 13:17:55 -0400

    fix view viewMatches

--- static_site/transferware-app/src/pages/viewMatches.js
+++ static_site/transferware-app/src/pages/viewMatches.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState  } from "react";
-import { useLocation } from "react-router-dom
+import { useLocation } from "react-router-dom";
 import MatchResults from "../components/matchResults";
 import Footer from "../components/footer";
 
