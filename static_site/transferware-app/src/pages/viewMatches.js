709e956dbdd2de1063c08ba4f54458e9edfabec4
bigelowk <112422927+bigelowk@users.noreply.github.com>
2025-04-14 11:43:24 -0400

    changed docker to fit local deployment

--- static_site/transferware-app/src/pages/viewMatches.js
+++ static_site/transferware-app/src/pages/viewMatches.js
@@ -19,7 +19,7 @@ function ViewMatches() {
         console.log("Fetched result_id:", resultId);
   
         if (resultId) {
-          document.location.assign(`http://transferware-ai.umd.umich.edu/survey/?analytics_id=${resultId}`);
+          document.location.assign(`http://localhost/survey/?analytics_id=${resultId}`);
         } else {
           console.warn("Result ID not available yet");
         }
